3c60473f06a163db18747fdce545812b4adfa8cc
naiarazoroza <93262292+naiarazoroza@users.noreply.github.com>
2021-10-29 13:56:18 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,51 @@
+basic.forever(function () {
+    if (input.soundLevel() > 120) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (input.soundLevel() > 100) {
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (input.soundLevel() > 80) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (input.soundLevel() < 60) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            `)
+    } else if (input.soundLevel() > 40) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
